f3c209447f74010343018b505e1f6a950c4356ba
Itwos In <itwosin@MacBookAir.lan>
2025-09-08 15:35:53 +0530

    Initial commit

--- gallereee-project/backend/src/models/Post.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const postSchema = new mongoose.Schema({
-  user:   { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  title:  { type: String, required: true },
-  image:  { type: String, required: true }
-}, { timestamps: true });
-
-export default mongoose.model("Post", postSchema);
--- gallereee-project/backend/src/services/api.js
+++
@@ -1,9 +0,0 @@
-// src/services/api.js
-import axios from "axios";
-
-// Change this to your real backend endpoint
-const BASE_URL = "http://localhost:5000/api";
-
-export const galleryAPI = {
-  getGalleries: () => axios.get(`${BASE_URL}/galleries`),
-};
